33c230ccb021d05e0673a1a6f1c030dc64490db8
ColorCC <xuwenlong.xinguan@gmail.com>
2020-12-22 23:19:22 +0800

    Update main.py

--- Segmentation/main.py
+++ Segmentation/main.py
@@ -16,7 +16,7 @@ filePath = path.join(getcwd(), 'Segmentation', 'resource')
 
 # 读取文件夹下所有图片
 def read_directory(images=[], imgType='.jpg'):
-    print('images loading...')
+    print('load images')
     imStack = []
     images = images if len(images) else listdir(filePath)
     for fullflname in images:
@@ -30,18 +30,19 @@ def read_directory(images=[], imgType='.jpg'):
 
 # 滤波器响应计算
 def filter_response(img, bank):
-    print('filter response...')
-    bankSize = bank.shape[2]
+    print('filter response')
+    # img => 灰度图 => numpy
     img = img.convert('L')
     [w, h] = img.size
     img = np.array(img).transpose()
+    bankSize = bank.shape[2]  # 49*49*38
     # 初始化 滤波器结果集
     responses = np.zeros((w, h, bankSize))  # w * h * 38
     for r in range(bankSize):
         # TODO!
-        # if r % 22 == 0:
-        responses[:, :, r] = signal.convolve2d(
-            img, bank[:, :, r],  mode='same')
+        if r % 22 == 0:
+            responses[:, :, r] = signal.convolve2d(
+                img, bank[:, :, r],  mode='same')
     responses = responses.reshape((w*h, bankSize))
     return responses
 
@@ -58,21 +59,17 @@ def pad_data(data, winSize):
 
 
 # 逐像素取大小为winSize*winSize的邻域数据
-# TODO!
-def gen_dataX(data, winSize):
-    x, y = data.shape
+# TODO! confirm
+def gen_window_data(data, winSize):
+    [x, y] = data.shape
     m = x-winSize//2*2
     n = y-winSize//2*2
-    res = np.zeros([m*n, winSize**2])
-    # print(m, n)  # 1000 667
-    k = 0
-    for i in range(winSize//2, m+winSize//2):
-        for j in range(winSize//2, n+winSize//2):
-            res[k, :] = np.reshape(
-                data[i-winSize//2:i+winSize//2+1, j-winSize//2:j+winSize//2+1].T, (1, -1))
-            k += 1
-    # print(k) # 667000
-    return res
+    windows = np.zeros((m, n, winSize**2))
+    for i in range(winSize//2, m):
+        for j in range(winSize//2, n):
+            windows[i, j, :] = data[i-winSize//2:i+winSize //
+                                    2+1, j-winSize//2:j+winSize//2+1].reshape(winSize**2,)
+    return windows
 
 
 # 计算原始图像 至 meanFeats 的距离，生成隶属度矩阵 labelIm
@@ -82,10 +79,10 @@ def quantizeFeats(featIm, meanFeats):
     w = featIm.shape[0]
     h = featIm.shape[1]
     [size, dim] = meanFeats.shape
-    labelIm = []
+    labelIm = np.zeros((w, h))
     for i in range(w):
         for j in range(h):
-            minDis = 0
+            minDis = -1
             for k in range(size):
                 dis = 0
                 for d in range(dim):
@@ -94,15 +91,15 @@ def quantizeFeats(featIm, meanFeats):
                     if minDis < 0 or dis < minDis:
                         labelIm[i, j] = k
                         minDis = dis
+    # 获取每一维的最大值
     return labelIm
 
 
 # 生成纹理词典
 def createTextons(imStack, bank, k):
-    print('textons start...')
+    print('textons start')
     textonsData = []
     for img in imStack:
-        # img => 灰度图 => numpy
         responses = filter_response(img, bank)
         # 结果集合并
         textonsData = np.concatenate(
@@ -121,29 +118,28 @@ def extractTextonHists(origIm, bank, textons, winSize):
     featIm = np.zeros((w, h, kCenter))  # 初始化
     responses = filter_response(origIm, bank)
     # 计算纹理响应到纹理集的距离，生成隶属度矩阵
-    # Calculates squared distance between two sets of points.
     distances = cdist(responses, textons, metric='euclidean')
     # 获取每一维的最大值
     indexs = distances.argmax(axis=1)
     feattexton = indexs.reshape(w, h)
     # 图像边界处理
     feattexton = pad_data(feattexton, winSize)
-    # 获取窗口数量
-    # TODO!
-    res = gen_dataX(feattexton, winSize)
-    print(res.shape)
+    print(feattexton.shape)
+    # 获取窗口
+    windowMap = gen_window_data(feattexton, winSize)
     # 统计纹理频率
-    for i in range(res.shape[0]):
-        # for j in range(res.shape[1]):
-            window = res[i]
-            frequency = Counter(window[:])
+    for i in range(windowMap.shape[0]):
+        for j in range(windowMap.shape[1]):
+            window = windowMap[i, j]
+            frequency = Counter(window)
+            print(frequency)
             for key in (frequency):  # 'Counter' object has no attribute 'shape'
                 # padding 补的 -1
                 # TODO!
                 if key != -1:
                     textonIndex = int(key)
                     count = frequency[key]
-                    featIm[i][textonIndex] = count
+                    featIm[i, j, textonIndex] = count
     return featIm
 
 
@@ -158,7 +154,7 @@ def compareSegmentations(origIm, bank, textons, winSize, numColorRegions, numTex
     colorCenter = KMeans(n_clusters=numColorRegions,  random_state=0).fit(
         colordata).cluster_centers_
     colorLabelIm = quantizeFeats(img, colorCenter)
-
+    
     featIm = extractTextonHists(origIm, bank, textons, winSize)
     w = featIm.shape[0]
     h = featIm.shape[1]
@@ -173,12 +169,12 @@ def main():
 
     # 1 加载图片
     # TODO!
-    imgSet = ['gumballs.jpg', 'snake.jpg', 'twins.jpg']
-    # imgSet = ['gumballs.jpg']
+    # imgSet = ['gumballs.jpg', 'snake.jpg', 'twins.jpg']
+    imgSet = ['gumballs.jpg']
     imStack = read_directory(imgSet)
     # 2 加载 filterBank
     bankPath = path.join(filePath, 'filterBank.mat')
-    filterBank = sio.loadmat(bankPath)['F']
+    filterBank = sio.loadmat(bankPath)['F']  # 49*49*38
     # 3 生成纹理词典
     textons = createTextons(imStack, filterBank, 5)
 
@@ -190,10 +186,10 @@ def main():
     [colorLabelIm, textureLabelIm] = compareSegmentations(
         img, filterBank, textons, 49, 10, 50)
 
-    print(colorLabelIm)
-    print(textureLabelIm)
-    print(colorLabelIm.shape)
-    print(textureLabelIm.shape)
+    # print(colorLabelIm)
+    # print(textureLabelIm)
+    # print(colorLabelIm.shape)
+    # print(textureLabelIm.shape)
 
 
 if __name__ == '__main__':
